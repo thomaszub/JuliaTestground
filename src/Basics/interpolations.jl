804cebc3ac8f4160e324928e6a32c287bdc2ebb0
Thomas Zub <thomas.zub@outlook.de>
2024-12-27 08:48:47 +0100

    Use non-deprecated interpolation functions

--- src/Basics/interpolations.jl
+++ src/Basics/interpolations.jl
@@ -6,8 +6,8 @@ x_coarse = 0:0.5:7
 y_fine = sin.(x_fine)
 y_coarse = sin.(x_coarse)
 
-linear = LinearInterpolation(x_coarse, y_coarse)
-cubic = CubicSplineInterpolation(x_coarse, y_coarse)
+linear = linear_interpolation(x_coarse, y_coarse)
+cubic = cubic_spline_interpolation(x_coarse, y_coarse)
 
 plot(x_fine, y_fine, label="fine")
 scatter!(x_coarse, y_coarse, label="coarse")
